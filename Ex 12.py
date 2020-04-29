b549a73120e9cbb075214c455398fb4dc750596f
Mohammad Amin Mamdouhi <63733898+M-Amin-Mamdouhi@users.noreply.github.com>
2020-04-29 18:01:28 +0430

    Add files via upload

--- Ex 12.py
+++ Ex 12.py
@@ -8,6 +8,13 @@ lb2=tkinter.Label(win,text='password')
 lb2.pack()
 en2=tkinter.Entry(win)
 en2.pack()
-btn=tkinter.Button(win,text='Enter')
+user='admin'
+password='12345'
+def checker():
+    if en1.get()==user and en2.get()==password:
+        btn.config(fg='green')
+    else:
+        btn.config(fg='red')
+btn=tkinter.Button(win,text='Enter',command=checker)
 btn.pack()
-
+str1=tkinter.StringVar()
